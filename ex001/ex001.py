64e8e8f0b37aae0d1517efea6c1e176e8f8315e1
Davi Prates <eudaviprates@gmail.com>
2025-06-20 23:24:44 -0300

    att

---
+++ ex001/ex001.py
@@ -0,0 +1,28 @@
+"""
+-- ###########################################################################
+-- ## 🐍 Programa Olá, Mundo!                                             ###
+-- ###########################################################################
+
+DESCRIÇÃO 📋:
+  Este script exibe a mensagem 'Olá, mundo!' na tela
+  do console.
+
+🎯 OBJETIVO:
+    1. Exibir a frase "Olá, mundo!" no terminal.
+    2. Utilização da função `print()`.
+
+
+🔍 O QUE FAZ:
+    [1] Utiliza a função nativa 'print()':
+      - É a função responsável por enviar dados para a saída padrão (a tela).
+
+    [2] Passa um argumento de texto (string):
+      - O texto 'Olá, mundo!' é fornecido como entrada para a função `print()`
+        e é o conteúdo que será exibido.
+
+----------------------------------------------------------------------------------------------------
+"""
+
+# ESCREVA SEU SCRIPT AQUI
+mensagem = 'Olá, mundo!'
+print(mensagem)
